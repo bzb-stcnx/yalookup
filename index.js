dfe33a35aa3c4be092549b69b7ef0e3f915a1ad7
bzb-stcnx <bzb-stcnx@users.noreply.github.com>
2015-10-21 23:48:51 +0200

    finalize unit tests and implement index.js

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
-'use strict'
 /* (c) Copyright 2015, bzb-stcnx
  * all rights reserved
  * SEE LICENSE IN ./LICENSE
  */
 
+module.exports = lookup
+
+var fs = require('fs')
+var path = require('path')
+var process = require('process')
+
+/**
+ * @description very light version of look-up
+ * @param {string} filename no support for glob, etc., just a simple file name
+ * @param {object} options, optional, defaults to { cwd: process.cwd() }
+ * only supports 'cwd'.
+ * @return {string} the first file path where filename was found
+ * starting from options.cwd upwards, or null if the file was not found
+ */
+function lookup (filename, options) {
+  var basedir
+
+  if (!isDefined(filename)) return null
+  basedir = isDefined(options) && isDefined(options.cwd)
+    ? options.cwd : process.cwd()
+
+  return _lookup(basedir, filename)
+}
+
+/**
+ * @description recursive look-up with custom arguments
+ * @param {string} basedir the directory to look into and above
+ * @param {string} filename
+ * @return {string} the first file path where filename was found,
+ * or null if the file was not found in or above path
+ */
+function _lookup (basedir, filename) {
+  var filepath = path.join(basedir, filename)
+  var parent
+  try {
+    fs.accessSync(filepath, fs.R_OK)
+    return filepath // found if no error thrown
+  } catch (err) {
+    // continue
+  }
+  parent = path.dirname(basedir)
+  if (!path.relative(parent, basedir).length) return null // root dir
+  return _lookup(parent, filename)
+}
+
+/**
+ * @param {any} val
+ * @return {boolean} true if val is not undefined or null
+ */
+function isDefined (val) {
+  return (typeof val !== 'undefined') && (val !== null)
+}
--- spec/spec.js
+++ spec/spec.js
@@ -6,23 +6,19 @@
 
 /* eslint-env jasmine */
 
-var lookup
-// var fs, path
 var createSpy
-var mockery
 
 beforeEach(function () {
-//  fs = require('fs')
-//  path = require('path')
-  lookup = require('../')
   createSpy = require('./support/create-spy.js')
-  mockery = require('mockery')
 })
 
 describe('yalookup:', function () {
+  var mockery
+  var lookup
   var mock
 
   beforeEach(function () {
+    mockery = require('mockery')
     mockery.enable({ useCleanCache: true })
     mock = createSpy({
       fs: [ 'accessSync' ],
@@ -33,6 +29,8 @@ describe('yalookup:', function () {
     mockery.registerMock('fs', mock.fs)
     mockery.registerMock('path', mock.path)
     mockery.registerMock('process', mock.process)
+    mockery.registerAllowable('../')
+    lookup = require('../')
   })
 
   afterEach(function () {
@@ -60,37 +58,59 @@ describe('yalookup:', function () {
     })
   })
 
-  describe('when given (filename: string, options: { cwd: string }) arguments,', function () {
-    var FILENAME, CWD
+  describe('when given (filename: string, options: { cwd: string }),', function () {
+    var FILENAME, CWD, FILEPATH, COUNT_START
+    var doLookup
 
     beforeEach(function () {
       FILENAME = '__filename__'
-      CWD = '/path/to/cwd'
+      CWD = '/path/to/cwd/'
+      FILEPATH = '/ya/found/it'
+      COUNT_START = 3
+      doLookup = function () {
+        return lookup(FILENAME, { cwd: CWD })
+      }
+      mock.path.join.and.returnValue(FILEPATH)
     })
 
-    it('process.cwd() is not called', function () {
-      expect(mock.process.cwd).not.toHaveBeenCalled()
-    })
-
-    describe('if the named file can be found directly in the given path,', function () {
+    describe('if the named file is in the given directory,', function () {
       var result
 
       beforeEach(function () {
+        // mock.fs.accessSync does not throw
+        result = doLookup()
       })
 
-      it('returns the path of the named file', function () {
-        expect(result).toBe(null)
+      it('returns the corresponding path of the named file', function () {
+        expect(result).toBe(FILEPATH)
+        expect(mock.fs.accessSync.calls.count()).toBe(1)
+      })
+
+      it('process.cwd() is not called', function () {
+        expect(mock.process.cwd).not.toHaveBeenCalled()
       })
     })
 
-    describe('if the named file can be found in a parent directory of the given path,', function () {
+    describe('if the named file is in a parent directory of the given directory,', function () {
+      var count
       var result
 
       beforeEach(function () {
+        count = COUNT_START
+        mock.path.relative.and.returnValue('/not/root/yet') // never reach top
+        mock.fs.accessSync.and.callFake(function () {
+          if (--count > 0) throw new Error() // do not throw on 0 -> file found
+        })
+        result = doLookup()
       })
 
-      it('returns the path of the named file', function () {
-        expect(result).toBe(null)
+      it('returns the corresponding path of the named file', function () {
+        expect(result).toBe(FILEPATH)
+        expect(mock.fs.accessSync.calls.count()).toBe(COUNT_START)
+      })
+
+      it('process.cwd() is not called', function () {
+        expect(mock.process.cwd).not.toHaveBeenCalled()
       })
     })
 
@@ -98,25 +118,37 @@ describe('yalookup:', function () {
       var result
 
       beforeEach(function () {
+        mock.fs.accessSync.and.throwError() // never find
+        mock.path.relative.and.returnValues('almost', 'there', '') // reach top
+        result = lookup(FILENAME, { cwd: CWD })
       })
 
       it('returns null', function () {
         expect(result).toBe(null)
       })
+
+      it('process.cwd() is not called', function () {
+        expect(mock.process.cwd).not.toHaveBeenCalled()
+      })
     })
   })
 
-  describe('when given only a (filename: string) argument,', function () {
+  describe('when given only (filename: string),', function () {
     var FILENAME, CWD
-    var result
 
     beforeEach(function () {
       FILENAME = '__filename__'
       CWD = '/path/to/cwd'
+      mock.process.cwd.and.returnValue(CWD)
+      lookup(FILENAME)
+    })
+
+    it('process.cwd() is called', function () {
+      expect(mock.process.cwd).toHaveBeenCalled()
     })
 
     it('defaults to process.cwd()', function () {
-      expect(result).toBe(null)
+      expect(mock.path.join).toHaveBeenCalledWith(CWD, FILENAME)
     })
   })
 })
